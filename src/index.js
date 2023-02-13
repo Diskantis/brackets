77a3d88026a577251b0917600ab4101ec3b67212
Diskantis <mn.zajkov@gmail.com>
2023-02-13 17:56:36 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketsPair = new Map(bracketsConfig);
+    const stack = [];
+    for (const symbol of str) bracketsPair.get(stack.at(-1)) === symbol ? stack.pop() : stack.push(symbol);
+
+    return stack.length === 0;
 }
+
